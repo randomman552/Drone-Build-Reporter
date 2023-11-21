018dfdcf8812c27f5d45ae0016bf81b551fd742c
George Grainger <ggrainger@labman.co.uk>
2023-11-21 22:14:30 +0000

    Add DroneContext models

--- reporters/reportercontext.go
+++ reporters/reportercontext.go
@@ -3,3 +3,48 @@ package reporters
 type ReporterContext struct {
 	Message string
 }
+
+type DroneContext struct {
+	Build        BuildContext
+	Commit       CommitContext
+	Repo         RepoContext
+	FailedStages []string
+	FailedSteps  []string
+}
+
+type BuildContext struct {
+	Action   string
+	Created  string
+	Event    string
+	Finished string
+	Link     string
+	Number   string
+	Parent   string
+	Started  string
+	Status   string
+	Trigger  string
+}
+
+type CommitContext struct {
+	Commit       string
+	After        string
+	Author       string
+	AuthorAvatar string
+	AuthorEmail  string
+	Before       string
+	Link         string
+	Message      string
+	Ref          string
+	SHA          string
+}
+
+type RepoContext struct {
+	Branch     string
+	Link       string
+	Name       string
+	Namespace  string
+	Owner      string
+	Private    bool
+	Type       string
+	Visibility string
+}
