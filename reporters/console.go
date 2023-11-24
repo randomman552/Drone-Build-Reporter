645c4b6347850090d63f00bd4136e61de4f3521c
George Grainger <ggrainger@labman.co.uk>
2023-11-24 16:26:54 +0000

    Add recursive field checking to console reporter

--- reporters/console.go
+++ reporters/console.go
@@ -10,13 +10,25 @@ type ConsoleReporter struct {
 	Config types.Config
 }
 
-func (c ConsoleReporter) Report(context types.DroneContext) {
-	typ := reflect.TypeOf(context)
+func Display(typ reflect.Type) {
 	fields := reflect.VisibleFields(typ)
 
 	for i := 0; i < len(fields); i++ {
 		field := fields[i]
+
+		if field.Type.Kind() == reflect.Struct {
+			fmt.Println("---", field.Type.Name(), "---")
+			Display(field.Type)
+			continue
+		}
+
 		value := reflect.ValueOf(field)
-		fmt.Println(field.Name, "-", value.String())
+		fmt.Println(field.Name, "-", value.Interface())
 	}
+}
+
+func (c ConsoleReporter) Report(context types.DroneContext) {
+	typ := reflect.TypeOf(context)
+
+	Display(typ)
 }
