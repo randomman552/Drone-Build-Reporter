e4330c5a929e198c42b02270573858344f84e914
George Grainger <ggrainger@labman.co.uk>
2023-11-25 11:26:42 +0000

    Use template for console reporter

--- reporters/console.go
+++ reporters/console.go
@@ -1,34 +1,25 @@
 package reporters
 
 import (
-	"fmt"
-	"reflect"
+	"os"
 	"reporter/types"
+	"text/template"
 )
 
 type ConsoleReporter struct {
 	Config types.Config
 }
 
-func Display(typ reflect.Type) {
-	fields := reflect.VisibleFields(typ)
-
-	for i := 0; i < len(fields); i++ {
-		field := fields[i]
-
-		if field.Type.Kind() == reflect.Struct {
-			fmt.Println("---", field.Type.Name(), "---")
-			Display(field.Type)
-			continue
-		}
-
-		value := reflect.ValueOf(field)
-		fmt.Println(field.Name, "-", value.Interface())
-	}
-}
-
 func (c ConsoleReporter) Report(context types.DroneContext) {
-	typ := reflect.TypeOf(context)
+	tplate, err := template.ParseFiles("templates/console.tmpl")
 
-	Display(typ)
+	if err != nil {
+		panic(err)
+	}
+
+	err = tplate.Execute(os.Stdout, context)
+
+	if err != nil {
+		panic(err)
+	}
 }
