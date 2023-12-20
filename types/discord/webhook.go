b2a767fd0ca6bead071da1b2d7ef0346f390f603
randomman552 <random.man552@protonmail.com>
2023-12-20 08:38:20 +0000

    Remove extra webhook variables

--- types/discord/webhook.go
+++ types/discord/webhook.go
@@ -1,7 +1,5 @@
 package discord
 
 type Webhook struct {
-	Content   string `json:"content"`
-	Username  string `json:"username"`
-	AvatarUrl string `json:"avatar_url"`
+	Content string `json:"content"`
 }
